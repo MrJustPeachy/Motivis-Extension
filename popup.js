a1204a05c69fa41acf7f246af2147ae7385cdda1
Dillon Pietsch <MrJustPeachy@users.noreply.github.com>
2018-05-24 22:50:35 -0500

    Updated the projected
    I revived the project and finally completed an alpha for that allows people to submit a form with their motivis information so I can hard code it in while I find a longer term, more substainable solution.

--- popup.js
+++ popup.js
@@ -4,17 +4,21 @@ var snapchatURL = chrome.extension.getURL("images/snapchat.png");
 var facebookURL = chrome.extension.getURL("images/social-facebook-icon.png");
 var githubURL = chrome.extension.getURL("images/github.png");
 
+function openForm(){
+    window.open('https://goo.gl/forms/ams8ht436efujegy1');
+}
+
 // Twitter Link
-$(".user-information").append('<a href="https://www.twitter.com/mrjustpeachy"> <img id="twitter-icon" src="twitter.png" alt="Twitter Logo" height="40" width="40"/> </a>');
-document.getElementById("twitter-icon").src = twitterURL;
+$(".navItems").append('<li><a target="_blank" href="https://www.twitter.com/mrjustpeachy"> <img id="twitter-icon" src="' + twitterURL + '" alt="Twitter Logo" height="30px" /> </a></li>');
 
 // Linkedin Link
-$(".user-information").append('<a href="https://www.linkedin.com/in/dillonpietsch/"> <img id="linkedin-icon" src="linkedin-logo.png" alt="Linkedin Logo" height="40" width="40"/> </a>');
-document.getElementById("linkedin-icon").src = linkedinURL;
+$(".navItems").append('<li><a target="_blank" href="https://www.linkedin.com/in/dillonpietsch/"> <img id="linkedin-icon" src="' + linkedinURL + '" alt="Linkedin Logo" height="30px" /> </a></li>');
 
 // Github Link
-$(".user-information").append('<a href="https://github.com/MrJustPeachy"> <img id="github-icon" src="snap.png" alt="Github Logo" height="40" width="40"/> </a>');
-document.getElementById("github-icon").src = githubURL;
+$(".navItems").append('<li> <a target="_blank" href="https://github.com/MrJustPeachy"> <img id="github-icon" src="' + githubURL + '" alt="Github Logo" height="30px" /> </a></li>');
+
+// Button for people to submit their own information
+$(".navItems").append('<li style="float: right; margin-right: 5%;"><input type="button" value="Get your social media buttons for your profile!" class="btn primary-btn" onclick="window.open(\'https://goo.gl/forms/ams8ht436efujegy1\')" /> </li>')
 
 /* WORK IN PROGRESS
 // Snapchat Link
